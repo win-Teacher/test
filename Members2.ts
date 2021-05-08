0f2ccc58beb2c8af6a91c6e384559170ebeb3e38
win-Teacher <mizaquavirgo@gmail.com>
2021-05-08 14:47:58 +0900

    sourceTreeより

---
+++ Members2.ts
@@ -0,0 +1,28 @@
+function setData(): void {
+
+
+    let members: string[] = [ //ダミーデータ
+        "ミズシマ", "キムタク", "フクヤマ", "モリタカ",
+        "ミズシマ2", "キムタク2", "フクヤマ2", "モリタカ2",
+        "ミズシマ3", "キムタク3", "フクヤマ3", "モリタカ3",
+    ];
+
+
+
+    localStorage.setItem("members", members.join(','));
+
+}
+
+function getData(): string[] {
+    let readData;
+    try {
+        console.log("try");
+        readData = (localStorage.getItem("members")).split(',');
+    } catch {
+        console.log("catch");
+        readData = [];
+    } finally {
+        console.log("finally");
+        return readData;
+    }
+}
